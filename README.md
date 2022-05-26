23e2d23bde2d835dc4c679ce77fdc9bd6433090e
Darren <splicereborn@gmail.com>
2022-05-26 22:37:10 +0100

    Write README, add code for callback.js

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # AsyncJSCrashCourse
+
+Followed tutorial: https://www.youtube.com/watch?v=ZYb_ZU8LNxs
+
+## Task description 
+
+Write a program that does the following tasks: 
+1. Place order (2 seconds)
+2. Cut the fruit (2 seconds)
+3. Add water and ice (1 second)
+4. Start the machine (1 second)
+5. Select container (2 seconds)
+6. Select toppings (3 seconds)
+7. Serve ice cream (2 seconds)
+
+These tasks will be solved using various methods, including callbacks, promises, async/await. We illustrate that repeated use of callbacks result in a callback hell and how other methods allow the code to be more readable and maintanable. 
---
+++ callback.js
@@ -0,0 +1,43 @@
+/* Ice cream business
+ * Receive order, prepare, serve
+ */
+
+let inventory = {
+    Fruits: ['strawberry', 'grape', 'banana', 'apple'],
+    Liquid: ['water', 'ice'],
+    Holder: ['cone', 'cup', 'stick'],
+    Toppings: ['chocolate', 'peanuts']
+}
+
+let order = (fruitName, callProduction) => {
+    setTimeout(() => {
+        console.log(`Order for ${inventory.Fruits[fruitName]}`)
+        callProduction()
+    }, 2000)
+}
+
+let production = () => {
+    setTimeout(() => {
+        console.log("Order received, starting production")
+        setTimeout(() => {
+            console.log("Fruit has been chopped")
+            setTimeout(() => {
+                console.log(`${inventory.Liquid[0]} and ${inventory.Liquid[1]} was added`)
+                setTimeout(() => {
+                    console.log("Starting the machine")
+                    setTimeout(() => {
+                        console.log(`Ice cream was placed on a ${inventory.Holder[0]}`)
+                        setTimeout(() => {
+                            console.log(`${inventory.Toppings[0]} was added`)
+                            setTimeout(() => {
+                                console.log("Ice cream was served")
+                            }, 2000)
+                        }, 3000)
+                    }, 2000)
+                }, 1000)
+            }, 1000)
+        }, 2000)
+    }, 0000)
+}
+
+order(0, production)
